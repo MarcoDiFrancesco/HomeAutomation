cd35159af164eaa160230c3821b0fef23455ba58
Marco Di Francesco <marcodifran@gmail.com>
2018-06-09 00:12:47 +0200

    Added support to \t to send data from arduino to processing

--- CasaDomotica.ino
+++ CasaDomotica.ino
@@ -7,10 +7,10 @@ dht11 sensore;
 
 char incoming;
 int looppp = 8;
-   
+
 void setup() {
   Serial.begin(9600);
-  
+
   pinMode(8, OUTPUT); // led n1
   pinMode(9, OUTPUT); // led n2
   pinMode(10, OUTPUT); // led n3
@@ -20,31 +20,40 @@ void setup() {
 }
 
 void loop() {
-  /*
-  led(8, 13); // firstPin, lastPin
+  //  led(8, 13); // firstPin, lastPin
 
   temperatureSensor(1); // connection with pin A0
+  // print humidity and temperature
+  
   temperatureSensor(2); // connection with pin A1
-  lightSensor(1); // connection with pin A2
-  lightSensor(2); // connection with pin A3
-  lightSensor(3); // connection with pin A4
+  // print humidity and temperature
 
-  Serial.print("Gas sensor: \t\t");
-  Serial.println(analogRead(A4));
-*/
-  if (Serial.available())  {
-    while(Serial.available()>0){
+  lightSensor(1); // connection with pin A2
+  // print light value
+  
+  lightSensor(2); // connection with pin A3
+  // print light value
+  
+  lightSensor(3); // connection with pin A4
+  // print light value
+  
+  Serial.print(analogRead(A4)); // print gas sensor value
+
+  Serial.println(); // print \n to close the string that will be sent 
+
+  if (Serial.available()) {
+    while (Serial.available() > 0) {
       incoming = Serial.read();
-      
-      if(incoming == '1'){
-        digitalWrite(looppp, HIGH);  
+
+      if (incoming == '1') {
+        digitalWrite(looppp, HIGH);
       } else {
-        digitalWrite(looppp, LOW);  
+        digitalWrite(looppp, LOW);
       }
-      if(looppp == 13){
+      if (looppp == 13) {
         looppp = 8;
       } else {
-        looppp = looppp + 1;  
+        looppp = looppp + 1;
       }
     }
   }
@@ -52,67 +61,39 @@ void loop() {
 }
 
 void temperatureSensor(int nSensor) {
-  Serial.print("Meteo sensor n");
-  Serial.print(nSensor);
-  Serial.print(", \t");
   if (nSensor == 1) {
-    sensore.read(DHT11_PIN1); // READ DATA
+    sensore.read(DHT11_PIN1); // read data
   } else if (nSensor == 2) {
-    sensore.read(DHT11_PIN2); // READ DATA
+    sensore.read(DHT11_PIN2); // read data
   } else {
-    Serial.print("Errore");
+    Serial.print("Error");
   }
-
-  // print data
-  Serial.print("Humidity: ");
-  Serial.print(sensore.humidity, 1); // Print umidità
-  Serial.print("%\tTemperature: ");
-  Serial.print(sensore.temperature, 1); // Print temperatura
-  Serial.println("°");
+  Serial.print(sensore.humidity, 1); // print humidity
+  Serial.print(sensore.temperature, 1); // print temperature
 }
 
-void led(int first, int last) {
+void led(int first, int last) { // led test
   for (int w = first; w <= last; w++) {
     digitalWrite(w, HIGH);
     delay(100);
   }
 
   for (int w = first; w <= last; w++) {
-    digitalWrite(w, LOW);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(w, LOW);
     delay(100);
   }
 }
 
-void lightSensor(int pin){
-  Serial.print("Light sensor");
+void lightSensor(int pin) {
   Serial.print(pin);
-  Serial.print(": \t\t");
-  if(pin == 1){
-    if(analogRead(A2)>500){      
-      Serial.print("Dark (");
-    } else {
-      Serial.print("Bright (");
-    }
+  if (pin == 1) {
     Serial.print(analogRead(A2));
-    Serial.println(")");
-  } else if (pin == 2){
-    if(analogRead(A3)>500){      
-      Serial.print("Dark (");
-    } else {
-      Serial.print("Bright (");
-    }
+  } else if (pin == 2) {
     Serial.print(analogRead(A3));
-    Serial.println(")");
-  } else if (pin == 3){
-    if(analogRead(A4)>500){      
-      Serial.print("Dark (");
-    } else {
-      Serial.print("Bright (");
-    }
+  } else if (pin == 3) {
     Serial.print(analogRead(A4));
-    Serial.println(")");
   } else {
-    Serial.println("Error reading light sensor");
+    Serial.print("Error");
   }
 }
 
