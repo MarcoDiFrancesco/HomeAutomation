7d12f81c6ccd19cc7d4247fd48fcfc44c0434cc8
Marco <marcodifran@gmail.com>
2018-06-11 00:05:35 +0200

    Delete checkLed.php

--- checkLed.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "../php/connect.php";
-$led = $_GET["led"];
-$query = "SELECT state FROM led where id=".$led;
-$sendquery = mysqli_query( $connect, $query );
-$takedata = mysqli_fetch_row($sendquery);
-$ledState = $takedata[0];
-echo $ledState;
-?>
