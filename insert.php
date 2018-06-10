5a68026035801c64ff7d4617452480b75fac02ec
Marco <marcodifran@gmail.com>
2018-06-11 00:05:47 +0200

    Delete insert.php

--- insert.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../php/connect.php";
-$ledState = $_POST["ledState"];
-$ledNumber = $_POST["ledNumber"];
-$query = "UPDATE led SET state='" . $ledState . "' WHERE id=" . $ledNumber;
-mysqli_query( $connect, $query );
-echo "Led " . $ledNumber . " aggiornato " . $ledState;
-?>
