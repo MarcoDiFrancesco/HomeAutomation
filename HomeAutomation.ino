6e6b2e0204b8a1acbeedaa2126ed16fcc3fa1511
Marco Di Francesco <marcodifran@gmail.com>
2018-06-23 10:45:17 +0200

    Changed base directory

--- HomeAutomation.ino
+++ HomeAutomation.ino
@@ -1,59 +1,92 @@
 #include <dht11.h>
 
-#define DHT11_PIN2 A1 // temperature sensor n1
-#define DHT11_PIN1 A0 // temperature sensor n2
+#define DHT11_PIN2 A4 // temperature sensor n1
+#define DHT11_PIN1 A6 // temperature sensor n2
 #define DHTTYPE DHT11
-dht11 sensore;
+dht11 sensorDHT;
 
 char incoming;
-int looppp = 8;
+int looppp = 38;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(8, OUTPUT); // led n1
-  pinMode(9, OUTPUT); // led n2
-  pinMode(10, OUTPUT); // led n3
-  pinMode(11, OUTPUT); // led n4
-  pinMode(12, OUTPUT); // led n5
-  pinMode(13, OUTPUT); // led n6
+  pinMode(38, OUTPUT); // white living room 
+  pinMode(39, OUTPUT); // white living room 
+  pinMode(40, OUTPUT); // white kitchen
+  pinMode(41, OUTPUT); // red
+  pinMode(42, OUTPUT); // blue
+  pinMode(43, OUTPUT); // yellow
+  pinMode(44, OUTPUT); // yellow
 }
 
 void loop() {
-  //  led(8, 13); // firstPin, lastPin
-
-  temperatureSensor(1); // connection with pin A0
+  Serial.print(analogRead(A0)); Serial.print("\t"); // sound sensor main room
+  Serial.print(analogRead(A1)); Serial.print("\t"); // gas sensor main room
+  Serial.print(analogRead(A2)); Serial.print("\t"); // ligt red (no)
+  Serial.print(analogRead(A3)); Serial.print("\t"); // gas sensor yellow
+  
+  temperatureSensor(1); 
+  // connection with pin A4
   // print humidity and temperature
   
-  temperatureSensor(2); // connection with pin A1
+  Serial.print(analogRead(A5)); Serial.print("\t"); // fire sensor red
+  
+  temperatureSensor(2); 
+  // connection with pin A6
   // print humidity and temperature
+  
+  Serial.print(analogRead(A7)); Serial.print("\t"); // fire sensor blue
+  Serial.print(analogRead(A8)); Serial.print("\t"); // smoke sensor blue
+  Serial.print(analogRead(A9)); Serial.print("\t"); // light sensor blue
+  Serial.print(analogRead(A10)); Serial.print("\t"); // fire sensor main room
 
-  Serial.print(analogRead(A2)); // print light sensor n1
-  Serial.print("\t");
-  
-  Serial.print(analogRead(A3)); // print light sensor n1
-  Serial.print("\t");
-  
-  Serial.print(analogRead(A4)); // print light sensor n1
-  Serial.print("\t");
-  
-//  Serial.print(analogRead(A5)); // print gas sensor value
-
-  Serial.println(); // print \n to close the string that will be sent 
+  Serial.println(); 
+  // print \n to close the string that will be sent 
 
   if (Serial.available()) {
     while (Serial.available() > 0) {
       incoming = Serial.read();
 
-      if (incoming == '1') {
-        digitalWrite(looppp, HIGH);
-      } else {
-        digitalWrite(looppp, LOW);
-      }
-      if (looppp == 13) {
-        looppp = 8;
-      } else {
-        looppp = looppp + 1;
+      if(looppp == 38){
+        if (incoming == '1') {
+          digitalWrite(38, HIGH);
+          digitalWrite(39, HIGH);           
+        } else {
+          digitalWrite(38, LOW);
+          digitalWrite(39, LOW);
+        }
+        looppp = 40;
+      } else if(looppp == 40){
+        if (incoming == '1') {
+         digitalWrite(40, HIGH);
+        } else {
+         digitalWrite(40, LOW);
+        }
+        looppp = 41;
+      }else if(looppp == 41){
+        if (incoming == '1') {
+         digitalWrite(41, HIGH);
+        } else {
+         digitalWrite(41, LOW);
+        }
+        looppp = 42;
+      }else if(looppp == 42){
+        if (incoming == '1') {
+         digitalWrite(42, HIGH);
+        } else {
+         digitalWrite(42, LOW);
+        }
+        looppp = 43;
+      }else if(looppp == 43){
+        if (incoming == '1') {
+         digitalWrite(43, HIGH);
+         digitalWrite(44, HIGH);
+        } else {
+         digitalWrite(43, LOW);
+         digitalWrite(44, LOW);
+        }
+        looppp = 38;
       }
     }
   }
@@ -62,28 +95,18 @@ void loop() {
 
 void temperatureSensor(int nSensor) {
   if (nSensor == 1) {
-    sensore.read(DHT11_PIN1); // read data
+    sensorDHT.read(DHT11_PIN1); // read data
   } else if (nSensor == 2) {
-    sensore.read(DHT11_PIN2); // read data
+    sensorDHT.read(DHT11_PIN2); // read data
   } else {
     Serial.print("Error");
   }
-  Serial.print(sensore.humidity, 1); // print humidity
+  Serial.print(sensorDHT.humidity, 1); 
+  // print humidity
   Serial.print("\t");
   
-  Serial.print(sensore.temperature, 1); // print temperature
+  Serial.print(sensorDHT.temperature, 1); 
+  // print temperature
   Serial.print("\t");
-}
-
-void led(int first, int last) { // led test
-  for (int w = first; w <= last; w++) {
-    digitalWrite(w, HIGH);
-    delay(100);
-  }
-
-  for (int w = first; w <= last; w++) {
-    digitalWrite(w, LOW);
-    delay(100);
-  }
 }
 
--- processing/processing.pde
+++ processing/processing.pde
@@ -4,10 +4,21 @@ Serial port;
 String input;
 
 void setup()  {
-  port = new Serial(this, Serial.list()[1], 9600); 
-  // port = new Serial(this, Serial.list()[2], 9600);
+  port = new Serial(this, Serial.list()[1], 9600); // windows
+  // port = new Serial(this, Serial.list()[2], 9600); // raspbian
   delay(5000);
 }
+
+/*
+Data order:
+  1 - temperature 1
+  2 - humidity 1
+  3 - temperature 2
+  4 - humidity 2
+  5 - light 1
+  6 - light 2
+  7 - light 3  
+*/
 
 void draw() {
   if(port.available()>0){
@@ -15,11 +26,12 @@ void draw() {
   }
   if(input != null){
     input = trim(input);
-    int dataSensors[] = {0,0,0,0,0,0,0,0,0,0};
+    int dataSensors[];
     dataSensors = int(split(input,'\t'));
     try{
-      loadStrings("https://www.marcodifrancesco.com/tesina/insertData.php?data1="+dataSensors[0]+"&data2="+dataSensors[1]+"&data3="+dataSensors[2]+"&data4="+dataSensors[3]+"&data5="+dataSensors[4]+"&data6="+dataSensors[5]); // remote
-  //  loadStrings("http://127.0.0.1/website/tesina/insertData.php?data1="+dataSensors[0]+"&data2="+dataSensors[1]+"&data3="+dataSensors[2]+"&data4="+dataSensors[3]+"&data5="+dataSensors[4]+"&data6="+dataSensors[5]); // local
+      // link to pass in POST the data
+      loadStrings("https://www.marcodifrancesco.com/tesina/insertData.php?data1="+dataSensors[0]+"&data2="+dataSensors[1]+"&data3="+dataSensors[2]+"&data4="+dataSensors[3]+"&data5="+dataSensors[4]+"&data6="+dataSensors[5]+"&data7="+dataSensors[6]+"&data8="+dataSensors[7]+"&data9="+dataSensors[8]+"&data10="+dataSensors[9]+"&data11="+dataSensors[10]+"&data12="+dataSensors[11]+"&data13="+dataSensors[12]); // remote
+//      loadStrings("http://127.0.0.1/tesina/insertData.php?data1="+dataSensors[0]+"&data2="+dataSensors[1]+"&data3="+dataSensors[2]+"&data4="+dataSensors[3]+"&data5="+dataSensors[4]+"&data6="+dataSensors[5]); // remote
     } catch (Exception e) {
       System.out.println("Exception occurred");
     }
@@ -27,21 +39,14 @@ void draw() {
 
   print(input+"\n");
 
-  int out[] = new int[7];
-  for(int led=1;led<=6;led++){
+  int out[] = new int[6];
+  for(int led=1;led<=5;led++){
+    // link to get from the query the led state
     String resultQuery[] = loadStrings("https://www.marcodifrancesco.com/tesina/checkLed.php?led="+led); // remote
-//    String resultQuery[] = loadStrings("http://127.0.0.1/website/tesina/checkLed.php?led="+led); // local
     int ledState = Integer.parseInt(resultQuery[0]);
     out[led]=int(ledState);
-    // print for testing
-    print(led + " - " + ledState);
-    if (resultQuery[0].equals("1") == true) {
-      println(" - ON");
-    } else {
-      println(" - OFF");
-    }
   }
-  String output = str(out[1])+str(out[2])+str(out[3])+str(out[4])+str(out[5])+str(out[6]);
-  port.write(output);
+  String output = str(out[1])+str(out[2])+str(out[3])+str(out[4])+str(out[5]);
+  port.write(output); // testing if works
   delay(1000);
 }
